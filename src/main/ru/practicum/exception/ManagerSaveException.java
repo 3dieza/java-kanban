db1dbfb14b43191cf399e7d4cf49f0029dcbf637
Тимур Низамов <tranizamov@sberbank.ru>
2025-01-06 14:01:38 +0300

    resolved conflict

--- src/main/ru/practicum/exception/ManagerSaveException.java
+++ src/main/ru/practicum/exception/ManagerSaveException.java
@@ -2,9 +2,10 @@ package ru.practicum.exception;
 
 import java.io.IOException;
 
-public class ManagerSaveException extends Exception {
+public class ManagerSaveException extends RuntimeException {
 
 
     public ManagerSaveException(String description, IOException e) {
+        super(description, e);
     }
 }
